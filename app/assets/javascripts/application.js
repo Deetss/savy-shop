939384cb7abb3cf1542cfc8944640cf23aeeee43
Deetss <dylan817@live.com>
2020-05-21 15:06:07 -0500

    add manifest for pwa features

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,12 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/service-worker.js', { scope: './' })
+      .then(function(reg) {
+        console.log('[Companion]', 'Service worker registered!');
+        console.log(reg);
+      });
+  }
