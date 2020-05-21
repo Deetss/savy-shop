818c32fc712d403673137e52429551120eb3baff
Deetss <dylan817@live.com>
2020-05-21 15:46:56 -0500

    Revert "add manifest for pwa features"
    This reverts commit 939384cb7abb3cf1542cfc8944640cf23aeeee43.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,12 +14,3 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
-
-if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/service-worker.js', { scope: './' })
-      .then(function(reg) {
-        console.log('[Companion]', 'Service worker registered!');
-        console.log(reg);
-      });
-  }
